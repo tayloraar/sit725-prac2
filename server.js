16bdb322817f49a002417a96d3b00d3475bc7095
Aaron <61723120+tayloraar@users.noreply.github.com>
2020-07-22 22:29:04 +1000

    Added a D20 roller in a get call

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 
-var app = express()
+var app = express();
 
-//respond with "hello world when a GET request is made to the homepage"
-app.get('/',function(request,response){
-    response.send('Hello Worlds')
-})
-//cheese
+//I've added this here to just play around and get confident with calling a function in the script using "GET somehwere else.
+function rollTheDice(min, max) {  
+    return Math.floor(Math.random() * (max - min) + min);
+ 
+}  
+
+//load the index.html under the directory public. This one was new to me.
+app.use(express.static(__dirname +'/public'));
+
+//I wanted to play around with nesting fucntions inside 'get' and adding some different layers.
+app.get('/D20', function(request,response){
+    let roll = rollTheDice(1,20);
+    if(roll == 20){
+        console.log(`NAT20! Critical Hit!`);
+    }else if (roll == 1){
+        console.log(`Oh no! Critical Fail. Time to roll a new character`);
+    } else {
+        console.log(`The D20 roll was ${roll}`);
+    };
+    
+});
+
 //start the server on port 3000
-app.listen(3000)
+app.listen(3000);
+
+//respond with "hello world when a GET request is made to the homepage" - I've left this in from todays online practical.
+// app.get('/',function(request,response){
+//      response.send('Hello Worlds')
+//})
